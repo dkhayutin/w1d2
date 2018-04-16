00413bcad0b97f2b17aa159c6d861d64c569fd17
dkhayutin <dan.khayutin@gmail.com>
2018-04-16 17:40:03 +0000

    getandPrinthTML2 again

--- getAndPrintHTML2.js
+++ getAndPrintHTML2.js
@@ -2,12 +2,7 @@ var https = require('https');
 
 function getAndPrintHTML (options) {
 
-var requestOptions = {
-    host: 'sytantris.github.io',
-    path: '/http-examples/step3.html'
-  };
-
-https.get(requestOptions, function(response) {
+https.get(options, function(response) {
 
   response.setEncoding('utf8');
   response.on('data', function (data) {
@@ -25,4 +20,10 @@ console.log('Respone Stream Complete');
   });
 });
 }
-getAndPrintHTML()
+
+var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step3.html'
+  };
+
+getAndPrintHTML(requestOptions)
